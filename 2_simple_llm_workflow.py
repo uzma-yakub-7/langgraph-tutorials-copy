5d1438730b706215a350ba168627f3b04c202dcf
Uzma Yakub <uzmayakub27@gmail.com>
2026-04-21 20:30:19 +0600

    Updated file

---
+++ 2_simple_llm_workflow.py
@@ -0,0 +1,25 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()
+model = ChatGroq(model="llama3-8b-8192")
+
+class State(TypedDict):
+    q: str
+    a: str
+
+def ask(s: State):
+    s["a"] = model.invoke(s["q"]).content
+    return s
+
+g = StateGraph(State)
+g.add_node("ask", ask)
+
+g.add_edge(START, "ask")
+g.add_edge("ask", END)
+
+app = g.compile()
+
+print(app.invoke({"q": "Distance to moon?"})["a"])
